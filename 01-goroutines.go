c4513a975b21b3ce9a975a63bb1053f4d925214c
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2022-03-26 10:33:08 -0300

    Adds example using go keyword

---
+++ 01-goroutines.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Cheguei")
+	go hello() // Não printa nada pois nesse codigo não há nada falando: ei função main, voce pode esperar até que a função hello termine de executar? 
+
+}
+
+func hello() {
+	fmt.Println("Startandoooo tudo")
+}
